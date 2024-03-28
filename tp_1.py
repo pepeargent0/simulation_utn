316488725a3fc8efc0570d8184a0bacccd5c87e9
pepeargentoo <dani_rc_22@hotmail.com>
2024-03-27 21:03:21 -0300

    estructura del proyecto

--- tp_1.py
+++ tp_1.py
@@ -1,31 +1,41 @@
 from random import randint
 import argparse
 import matplotlib as plt
-parser = argparse.ArgumentParser(description='Calculadora, suma/resta/multiplica a y b')
-parser.add_argument('-c', '--corridas', type=int, help='Corridas')
+
+
+class RouletteSimulation:
+    def __init__(self, number_runs, number_rolls, chosen_number):
+        self.results = []
+        self.number_runs = number_runs
+        self.number_rolls = number_rolls
+        self.chosen_number = chosen_number
+        self.frecuency_absolute = 0
+
+    def simulation(self):
+        for run in range(self.number_runs):
+            self.results.append({})
+            for rolls in range(self.number_rolls):
+                _key = randint(0, 36)
+                if _key == self.chosen_number:
+                    self.frecuency_absolute += 1
+                if _key in self.results[run].keys():
+                    self.results[run][_key] += 1
+                else:
+                    self.results[run][_key] = 1
+        print('frecuencia Absoluta:', self.frecuency_absolute)
+        print('freciencia relativa: ', self.frecuency_absolute / (self.number_runs * self.number_rolls))
+
+
+parser = argparse.ArgumentParser(description='Simulacion de ruleta')
+parser.add_argument('-c', '--numero_corridas', type=int, help='Corridas')
 parser.add_argument('-n', '--numero_tiradas', type=int, help='numero de tiradas')
 parser.add_argument('-e', '--numero_eleguido', type=int, help='numero de eleguido')
 
 args = parser.parse_args()
-
-
-
-
-if args.corridas and args.numero_tiradas and args.numero_eleguido:
-    data = []
-    frecuencia_absoluta = 0
-    for corrida in range(args.corridas):
-        data.append({})
-        for tirada in range(args.numero_tiradas):
-            key = randint(0, 36)
-            if key == args.numero_eleguido:
-                frecuencia_absoluta += 1
-            if key in data[corrida].keys():
-                data[corrida][key] += 1
-            else:
-                data[corrida][key] = 1
-    print('frecuencia Absoluta:', frecuencia_absoluta)
-    print('freciencia relativa: ', frecuencia_absoluta/(args.numero_tiradas*args.corridas))
+if ((args.numero_corridas and args.numero_tiradas and args.numero_eleguido)
+        and (args.numero_corridas >= 0 and args.numero_tiradas >= 0 and args.numero_eleguido >= 0)):
+    simulation = RouletteSimulation(args.numero_corridas, args.numero_tiradas, args.numero_eleguido)
+    simulation.simulation()
 else:
     print(' Los valores -c -n -e son obligatorios')
     exit()
