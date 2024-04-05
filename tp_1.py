47f1f4ae0455263e0c0c96f624533a272c15aac4
pepeargentoo <dani_rc_22@hotmail.com>
2024-04-05 15:19:20 -0300

    avances en tp

--- tp_1.py
+++ tp_1.py
@@ -1,30 +1,27 @@
-from random import randint
-import numpy as np
 import argparse
+import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-class RouletteSimulation:
-    def __init__(self, cantidad_tiradas, number_rolls, numero_elegido):
-        self.results = []
-        self.cantidad_tiradas = cantidad_tiradas
-        self.cantidad_tiradas = cantidad_tiradas
-        self.numero_elegido = numero_elegido
-        self.frequency_absolute = 0
-        self.frecuencias_relativas = []
-        self.average_values = []
+class Ruleta:
+    def __init__(self, _numero_elegido, _cantidad_tiradas, _cantidad_corridas):
+        self.numero_elegido = _numero_elegido
+        self.cantidad_tiradas = _cantidad_tiradas
+        self.cantidad_corridas = _cantidad_corridas
+        self.resultados = self.simular_corridas()
 
-    def simulation(self):
-        resultados = [randint(0, 36) for _ in range(self.cantidad_tiradas)]
+    def simular_corrida(self):
+        resultados = [random.randint(0, 36) for _ in range(self.cantidad_tiradas)]
+        _contador = 0
         frecuencias_relativas = []
-        contador = 0
         promedios = []
         varianzas = []
         desvios = []
         for i, tirada in enumerate(resultados, start=1):
             if tirada == self.numero_elegido:
-                contador += 1
-            frecuencia_relativa = contador / i
+                _contador += 1
+            frecuencia_relativa = _contador / i
             frecuencias_relativas.append(frecuencia_relativa)
             promedio = sum(resultados[:i]) / i
             promedios.append(promedio)
@@ -32,24 +29,55 @@ class RouletteSimulation:
             varianzas.append(varianza)
             desvio = np.sqrt(varianza)
             desvios.append(desvio)
+        return frecuencias_relativas, promedios, varianzas, desvios
 
+    def simular_corridas(self):
+        return [self.simular_corrida() for _ in range(self.cantidad_corridas)]
 
-    def get_frequencies(self):
-        frequencies = {'FR': [], 'FRE': [], 'FRN': []}
-        for run in self.average_values:  # Iterar sobre las frecuencias relativas por tirada en cada corrida
-            total_rolls = sum(run)
-            if total_rolls != 0:
-                fr = run[self.chosen_number] / total_rolls  # Calcular la frecuencia relativa para el número elegido
-            else:
-                fr = 0.0
-            fre = 1 / 36
-            frn = fre / self.number_runs
-            frequencies['FR'].append(fr)
-            frequencies['FRE'].append(fre)
-            frequencies['FRN'].append(frn)
-        return frequencies
+    def graficar_frecuencia_relativa(self):
+        plt.figure(figsize=(20, 10))
+        for i, corrida in enumerate(self.resultados, start=1):
+            plt.plot(corrida)
+            plt.plot(range(self.cantidad_tiradas), corrida[0], label=f'Corrida {i}')
+        plt.xlabel('Cantidad de corridas')
+        plt.ylabel('Frecuencia Relativa')
+        plt.title('Frecuencia Relativa')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(self.resultados), fancybox=True, shadow=True)
+        plt.grid(True)
+        plt.show()
 
+    def graficar_promedio(self):
+        plt.figure(figsize=(20, 10))
+        for i, corrida in enumerate(self.resultados, start=1):
+            plt.plot(range(self.cantidad_tiradas), corrida[1], label=f'Corrida {i}')
+        plt.xlabel('Cantidad de tiradas')
+        plt.ylabel('Promedio')
+        plt.title('Promedio')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(self.resultados), fancybox=True, shadow=True)
+        plt.grid(True)
+        plt.show()
 
+    def graficar_varianza(self):
+        plt.figure(figsize=(20, 10))
+        for i, corrida in enumerate(self.resultados, start=1):
+            plt.plot(range(self.cantidad_tiradas), corrida[2], label=f'Corrida {i}')
+        plt.xlabel('Cantidad de tiradas')
+        plt.ylabel('Varianza')
+        plt.title('Varianza')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(self.resultados), fancybox=True, shadow=True)
+        plt.grid(True)
+        plt.show()
+
+    def graficar_desvio(self):
+        plt.figure(figsize=(20, 10))
+        for i, corrida in enumerate(self.resultados, start=1):
+            plt.plot(range(self.cantidad_tiradas), corrida[3], label=f'Corrida {i}')
+        plt.xlabel('Cantidad de tiradas')
+        plt.ylabel('Desvío')
+        plt.title('Desvío')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(self.resultados), fancybox=True, shadow=True)
+        plt.grid(True)
+        plt.show()
 
 
 parser = argparse.ArgumentParser(description='Simulacion de ruleta')
@@ -58,34 +86,21 @@ parser.add_argument('-n', '--numero_tiradas', type=int, help='numero de tiradas'
 parser.add_argument('-e', '--numero_eleguido', type=int, help='numero de eleguido')
 
 args = parser.parse_args()
-number_runs, number_rolls, chosen_number = args.numero_corridas, args.numero_tiradas, args.numero_eleguido
+cantidad_corridas, cantidad_tiradas, numero_elegido = args.numero_corridas, args.numero_tiradas, args.numero_eleguido
 
-if all(isinstance(arg, int) and arg >= 0 for arg in [number_runs, number_rolls, chosen_number]):
-    simulation = RouletteSimulation(number_runs, number_rolls, chosen_number)
-    results, absolute_frequency, relative_frequency = simulation.simulation()
-    frequencies = simulation.get_frequencies()
-    print(frequencies['FR'])
-    plt.figure(figsize=(10, 6))
-    for i, fr_list in enumerate(frequencies['FR']):
-        plt.plot(i+1, fr_list, label=f'Tirada {i + 1}', linestyle = 'dotted')
-    plt.xlabel('Corridas')
-    plt.ylabel('Frecuencia Relativa')
-    plt.title('Evolución de la Frecuencia Relativa del Número Elegido')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    """
-    for i, fr in enumerate(frs):
-        plt.plot(range(1, number_runs + 1), fr, label=f'Tirada {i + 1}')
-    plt.xlabel('Corridas')
-    plt.ylabel('Frecuencia Relativa')
-    plt.title('Evolución de la Frecuencia Relativa del Número Elegido')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    """
+if not 0 <= numero_elegido <= 36:
+    print('-e debe ser un entero positivo entre 0 y 36')
+    exit()
+
+if all(isinstance(arg, int) and arg >= 0 for arg in [cantidad_corridas, cantidad_tiradas, numero_elegido]):
+    ruleta = Ruleta(_numero_elegido=numero_elegido,_cantidad_tiradas=cantidad_tiradas, _cantidad_corridas=cantidad_corridas)
+    ruleta.graficar_frecuencia_relativa()
+    ruleta.graficar_promedio()
+    ruleta.graficar_varianza()
+    ruleta.graficar_desvio()
+
 else:
     print('Los valores -c -n -e son obligatorios.')
     print('-c debe ser un entero positivo')
     print('-n debe ser un entero positivo')
-    print('-e debe ser un entero positivo')
+    print('-e debe ser un entero positivo entre 0 y 36')
